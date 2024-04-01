325eb7da0f84bf6673a38bcdc4ac91ff00f4ebd6
Christian Helmich <kagekirin@gmail.com>
2024-04-01 12:14:57 +0900

    feature: implement prototype for Alias|Wavefront OBJ export functionality (feature/prototype-obj-export) [#2]
    - **build (Runtime): add assembly definition**
     
    
    - **build (Editor): add assembly definition**
     
    
    - **feature (Runtime): define IWavefrontOBJWriter interface**
     
    
    - **feature (Runtime): define IWavefrontMTLWriter interface**
     
    
    - **feature (Runtime): implement IWavefrontOBJWriter as WavefrontOBJWriter**
     
    
    - **feature (Runtime): implement IWavefrontMTLWriter as WavefrontMTLWriter**
     
    
    - **feature (Editor): implement 'FrozenAPE/Export Wavefront OBJ...' menu functionality**
     
    
    - **build (Runtime): add assembly definition**
     
    
    - **build (Editor): add assembly definition**
     
    
    - **feature (Runtime): define IWavefrontOBJWriter interface**
     
    
    - **feature (Runtime): define IWavefrontMTLWriter interface**
     
    
    - **feature (Runtime): implement IWavefrontOBJWriter as WavefrontOBJWriter**
     
    
    - **feature (Runtime): implement IWavefrontMTLWriter as WavefrontMTLWriter**
     
    
    - **feature (Editor): implement 'FrozenAPE/Export Wavefront OBJ...' menu functionality**

---
+++ Editor/FrozenAPE.Menu.cs
@@ -0,0 +1,125 @@
+using System;
+using System.IO;
+using System.Text;
+using FrozenAPE;
+using UnityEditor;
+using UnityEngine;
+
+namespace FrozenAPE
+{
+    public class FrozenAPE_Menu : EditorWindow
+    {
+        [MenuItem("FrozenAPE/Export as Wavefront OBJ...")]
+        [MenuItem("GameObject/FrozenAPE/Export as Wavefront OBJ...")]
+        [MenuItem("Assets/FrozenAPE/Export as Wavefront OBJ...")]
+        private static void ExportOBJ(MenuCommand menuCommand)
+        {
+            IWavefrontOBJWriter objWriter = new WavefrontOBJWriter();
+            IWavefrontMTLWriter mtlWriter = new WavefrontMTLWriter();
+
+            if (Selection.activeObject == null)
+            {
+                Debug.LogError("Nothing selected! Please select a GameObject.");
+                return;
+            }
+
+            GameObject go = (GameObject)Selection.activeObject;
+            if (go != null)
+            {
+                foreach (var meshFilter in go.GetComponentsInChildren<MeshFilter>(true))
+                {
+                    if (meshFilter.sharedMesh == null)
+                        continue;
+
+                    var meshRenderer = meshFilter.gameObject.GetComponent<MeshRenderer>();
+
+                    var targetPathObj = EditorUtility.SaveFilePanel(
+                        "Export Wavefront OBJ",
+                        null,
+                        $"{meshFilter.gameObject.name}.obj",
+                        "obj"
+                    );
+                    if (string.IsNullOrEmpty(targetPathObj))
+                    {
+                        Debug.LogError("Nothing selected! Please select a file to write to.");
+                        continue;
+                    }
+
+                    var targetPathMtl = EditorUtility.SaveFilePanel(
+                        "Export Wavefront MTL",
+                        null,
+                        $"{meshFilter.gameObject.name}.mtl",
+                        "mtl"
+                    );
+                    if (string.IsNullOrEmpty(targetPathMtl))
+                    {
+                        Debug.LogError("Nothing selected! Please select a file to write to.");
+                        continue;
+                    }
+
+                    var materials = meshRenderer != null ? meshRenderer.sharedMaterials : Array.Empty<Material>();
+                    var sbObj = objWriter.WriteOBJ(Path.GetFileNameWithoutExtension(targetPathObj), meshFilter.sharedMesh, materials, new StringBuilder());
+                    var sbMtl = mtlWriter.WriteMTL(Path.GetFileNameWithoutExtension(targetPathMtl), materials, new StringBuilder());
+                    File.WriteAllText(targetPathObj, sbObj.ToString());
+                    File.WriteAllText(targetPathMtl, sbMtl.ToString());
+                }
+
+                foreach (var skinnedMeshRenderer in go.GetComponentsInChildren<SkinnedMeshRenderer>(true))
+                {
+                    if (skinnedMeshRenderer.sharedMesh == null)
+                        continue;
+
+                    var targetPathObj = EditorUtility.SaveFilePanel(
+                        "Export Wavefront OBJ",
+                        null,
+                        $"{skinnedMeshRenderer.gameObject.name}.obj",
+                        "obj"
+                    );
+                    if (string.IsNullOrEmpty(targetPathObj))
+                    {
+                        Debug.LogError("Nothing selected! Please select a file to write to.");
+                        continue;
+                    }
+
+                    var targetPathMtl = EditorUtility.SaveFilePanel(
+                        "Export Wavefront MTL",
+                        null,
+                        $"{skinnedMeshRenderer.gameObject.name}.mtl",
+                        "mtl"
+                    );
+                    if (string.IsNullOrEmpty(targetPathMtl))
+                    {
+                        Debug.LogError("Nothing selected! Please select a file to write to.");
+                        continue;
+                    }
+
+                    var sbObj = objWriter.WriteOBJ(
+                        Path.GetFileNameWithoutExtension(targetPathObj),
+                        skinnedMeshRenderer.sharedMesh,
+                        skinnedMeshRenderer.sharedMaterials,
+                        new StringBuilder()
+                    );
+                    var sbMtl = mtlWriter.WriteMTL(Path.GetFileNameWithoutExtension(targetPathMtl), skinnedMeshRenderer.sharedMaterials, new StringBuilder());
+                    File.WriteAllText(targetPathObj, sbObj.ToString());
+                    File.WriteAllText(targetPathMtl, sbMtl.ToString());
+                }
+                return;
+            }
+
+            Mesh mesh = (Mesh)Selection.activeObject;
+            if (mesh != null)
+            {
+                var targetPathObj = EditorUtility.SaveFilePanel("Export Wavefront OBJ", null, $"{mesh.name}.obj", "obj");
+                if (string.IsNullOrEmpty(targetPathObj))
+                {
+                    Debug.LogError("Nothing selected! Please select a file to write to.");
+                    return;
+                }
+
+                var sbObj = objWriter.WriteOBJ(Path.GetFileNameWithoutExtension(targetPathObj), mesh, Array.Empty<Material>(), new StringBuilder());
+                File.WriteAllText(targetPathObj, sbObj.ToString());
+                return;
+            }
+        }
+    }
+}
---
+++ Runtime/WavefrontOBJWriter.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace FrozenAPE
+{
+    public class WavefrontOBJWriter : IWavefrontOBJWriter
+    {
+        public virtual StringBuilder WriteOBJ(string name, Mesh mesh, Material[] materials, StringBuilder sb)
+        {
+            sb.AppendLine($"o {name}");
+
+            sb.AppendLine().AppendLine("# materials");
+            sb.AppendLine($"mtllib {name}.mtl");
+
+            sb.AppendLine().AppendLine("# vertices");
+            foreach (var v in mesh.vertices)
+            {
+                sb.AppendLine($"v {v.x} {v.y} {v.z}");
+            }
+
+            sb.AppendLine().AppendLine("# normals");
+            foreach (var vn in mesh.normals)
+            {
+                sb.AppendLine($"vn {vn.x} {vn.y} {vn.z}");
+            }
+
+            sb.AppendLine().AppendLine("# texcoords");
+            foreach (var vt in mesh.uv)
+            {
+                sb.AppendLine($"vt {vt.x} {vt.y}");
+            }
+
+            for (int submeshIndex = 0; submeshIndex < mesh.subMeshCount; submeshIndex++)
+            {
+                var desc = mesh.GetSubMesh(submeshIndex);
+                var faceTag = desc.topology switch
+                {
+                    MeshTopology.Triangles => "f",
+                    MeshTopology.Quads => "f",
+                    MeshTopology.Lines => "l",
+                    MeshTopology.LineStrip => "l",
+                    MeshTopology.Points => "p",
+                    _ => "f",
+                };
+                var elementsPerLine = desc.topology switch
+                {
+                    MeshTopology.Triangles => 3,
+                    MeshTopology.Quads => 4,
+                    MeshTopology.Lines => 2,
+                    MeshTopology.LineStrip => desc.indexCount,
+                    MeshTopology.Points => 1,
+                    _ => 1,
+                };
+                Material mat = materials.ElementAtOrDefault(submeshIndex);
+
+                sb.AppendLine().AppendLine($"# submesh {submeshIndex}").AppendLine($"g {mesh.name}_{submeshIndex}");
+
+                if (mat != null)
+                    sb.AppendLine($"usemtl {mat.name}");
+
+                for (int i = 0; i < desc.indexCount; i += elementsPerLine)
+                {
+                    sb.Append($"{faceTag}");
+                    for (int x = 0; x < elementsPerLine; x++)
+                    {
+                        sb.Append($" {1 + mesh.triangles[desc.indexStart + i + x]}"); // indices are 1 based
+                    }
+                    sb.AppendLine("");
+                }
+            }
+
+            return sb;
+        }
+    }
+}
