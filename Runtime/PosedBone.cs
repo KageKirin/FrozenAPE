a2ebd590ec533abbdd6db39ec4960edef6211760
Christian Helmich <kagekirin@gmail.com>
2024-04-10 14:16:51 +0900

    feature (Runtime): add PosedBone data structure (feature/pose-data-structure) [#5]
    - **build: add dependency on com.unity.serialization v3.1.1**
    - **build (Runtime): add assembly definition reference to Unity.Serialization**
    - **build (Editor): add assembly definition reference to Unity.Serialization**
    - **feature (Runtime): add PosedBone data structure**
    - **feature (Runtime): add PosedBoneContainer**
    
    - **build: add dependency on com.unity.serialization v3.1.1**
    - **build (Runtime): add assembly definition reference to Unity.Serialization**
    - **build (Editor): add assembly definition reference to Unity.Serialization**
    - **feature (Runtime): add PosedBone data structure**
    - **feature (Runtime): add PosedBoneContainer**

---
+++ Runtime/PosedBone.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using Unity.Mathematics;
+
+#nullable enable
+
+namespace FrozenAPE
+{
+    [Serializable]
+    public struct PosedBone
+    {
+        /// <summary>
+        /// bone name
+        /// target bone referenced by name
+        /// </summary>
+        public string targetBone;
+
+        /// <summary>
+        /// bone position
+        /// </summary>
+        public double3? position;
+
+        /// <summary>
+        /// bone rotation (Euler angles)
+        /// </summary>
+        public double3? rotation;
+
+        /// <summary>
+        /// bone scaling
+        /// CANNOT BE 0
+        /// </summary>
+        public double3? scaling;
+
+        /// <summary>
+        /// bone position offset
+        /// offset to default position
+        /// </summary>
+        public double3? positionOffset;
+
+        /// <summary>
+        /// bone rotation (Euler angles)
+        /// angular offset to default orientation
+        /// </summary>
+        public double3? rotationOffset;
+
+        /// <summary>
+        /// bone scaling factor
+        /// **FACTOR**
+        /// CANNOT BE 0
+        /// </summary>
+        public double3? scalingFactor;
+    }
+
+    [Serializable]
+    public class PosedBoneContainer
+    {
+        public List<PosedBone> bones = new();
+    }
+}
