726c35d7c7d3f81272bae30d42ecc871ef84ee8d
Christian Helmich <kagekirin@gmail.com>
2024-04-11 14:54:38 +0900

    refactor (WavefrontOBJWriter): simplify vertex/normal handedness transforms to mere vector multiplications (refactor/optimize-obj-handedness-transform) [#15]
    - **refactor (WavefrontOBJWriter): simplify vertex handedness transform to a mere vector multiplication**
     reason: matrix multiplication is overkill for this.
     
    
    - **refactor (WavefrontOBJWriter): simplify normal handedness transform to a mere vector multiplication**
     reason: matrix multiplication is overkill for this.
     
    
    - **refactor (WavefrontOBJWriter): simplify vertex handedness transform to a mere vector multiplication**
     reason: matrix multiplication is overkill for this.
     
    
    - **refactor (WavefrontOBJWriter): simplify normal handedness transform to a mere vector multiplication**
     reason: matrix multiplication is overkill for this.

--- Runtime/WavefrontOBJWriter.cs
+++ Runtime/WavefrontOBJWriter.cs
@@ -12,12 +12,7 @@ namespace FrozenAPE
         /// Unity is using a left-handed coordinate system
         /// whereas OBJ expects right-handed coordinates.
         /// </summary>
-        static float4x4 kmLeftToRightHandedness = math.float4x4(-1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
-        static float3x3 kmLeftToRightHandedness_normal = math.float3x3(
-            kmLeftToRightHandedness.c0.xyz,
-            kmLeftToRightHandedness.c1.xyz,
-            kmLeftToRightHandedness.c2.xyz
-        );
+        static float3 kvLeftToRightHandedness = math.float3(-1, 1, 1);
 
         public virtual StringBuilder WriteOBJ(string name, Mesh mesh, Material[] materials, StringBuilder sb)
         {
@@ -29,14 +24,14 @@ namespace FrozenAPE
             sb.AppendLine().AppendLine("# vertices");
             foreach (var v in mesh.vertices)
             {
-                var v_rh = math.mul(kmLeftToRightHandedness, math.float4(v, 1)).xyz;
+                var v_rh = kvLeftToRightHandedness * v;
                 sb.AppendLine($"v {v_rh.x} {v_rh.y} {v_rh.z}");
             }
 
             sb.AppendLine().AppendLine("# normals");
             foreach (var vn in mesh.normals)
             {
-                var vn_rh = math.mul(kmLeftToRightHandedness_normal, vn);
+                var vn_rh = kvLeftToRightHandedness * vn;
                 sb.AppendLine($"vn {vn_rh.x} {vn_rh.y} {vn_rh.z}");
             }
 
