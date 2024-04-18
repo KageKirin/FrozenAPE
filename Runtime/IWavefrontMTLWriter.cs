f8ed5fd7a5777adb0b66ec42eee092780f101781
Chris 'KageKirin' Helmich <kagekirin@gmail.com>
2024-04-18 20:42:23 +0900

    refactor (Runtime): simplify IWavefrontMTLWriter.WriteMTL() interface
    reason: taking and returning StringBuilder exposes an implementation detail, that is technically irrelevant for the interface.

--- Runtime/IWavefrontMTLWriter.cs
+++ Runtime/IWavefrontMTLWriter.cs
@@ -1,11 +1,10 @@
 using System;
-using System.Text;
 using UnityEngine;
 
 namespace FrozenAPE
 {
     public interface IWavefrontMTLWriter
     {
-        StringBuilder WriteMTL(string name, Material[] materials, StringBuilder sb);
+        string WriteMTL(string name, Material[] materials);
     }
 }
