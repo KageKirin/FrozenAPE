be3ccb96ff802c7830cae7a2e575d68166c9dd79
Chris 'KageKirin' Helmich <kagekirin@gmail.com>
2024-04-18 20:43:08 +0900

    doc (Runtime): document IWavefrontMTLWriter interface

--- Runtime/IWavefrontMTLWriter.cs
+++ Runtime/IWavefrontMTLWriter.cs
@@ -5,6 +5,12 @@ namespace FrozenAPE
 {
     public interface IWavefrontMTLWriter
     {
+        /// <summary>
+        /// create the MTL (ascii) material library from supplied materials
+        /// </summary>
+        /// <param name="name">name given to this material library. Note: it must match the name provided to the OBJ file as well as the filename written to disc</param>
+        /// <param name="materials">array of Unity Material data, usually obtained from `MeshRenderer.sharedMaterials` or `SkinnedMeshRenderer.sharedMaterials`</param>
+        /// <returns>string containing the MTL (ascii) material library</returns>
         string WriteMTL(string name, Material[] materials);
     }
 }
