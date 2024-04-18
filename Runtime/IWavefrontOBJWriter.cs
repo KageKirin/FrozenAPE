c992c6aa3c345b75266a3e74927ef6606543a27b
Chris 'KageKirin' Helmich <kagekirin@gmail.com>
2024-04-18 20:42:54 +0900

    doc (Runtime): document IWavefrontOBJWriter interface

--- Runtime/IWavefrontOBJWriter.cs
+++ Runtime/IWavefrontOBJWriter.cs
@@ -5,6 +5,13 @@ namespace FrozenAPE
 {
     public interface IWavefrontOBJWriter
     {
+        /// <summary>
+        /// create the OBJ (ascii) representation from supplied mesh and materials
+        /// </summary>
+        /// <param name="name">name given to this model. Note: it must match the name provided to the MTL file as well as the filename written to disc</param>
+        /// <param name="mesh">Unity Mesh data, usually obtained from `MeshFilter.sharedMesh` or `SkinnedMeshRenderer.sharedMesh`</param>
+        /// <param name="materials">array of Unity Material data, usually obtained from `MeshRenderer.sharedMaterials` or `SkinnedMeshRenderer.sharedMaterials`</param>
+        /// <returns>string containing the OBJ (ascii) representation</returns>
         string WriteOBJ(string name, Mesh mesh, Material[] materials);
     }
 }
