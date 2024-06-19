ff83c1d7a0445c21bbb9f649d18c0bdb6cb4d4cf
Chris 'KageKirin' Helmich <kagekirin@gmail.com>
2024-06-19 11:29:54 +0900

    feature (Editor): implement 'FrozenAPE/Insta Pose, Freeze and Export from JSON...' menu functionality

---
+++ Editor/FrozenAPE.InstaFreezePoseExport.Menu.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using FrozenAPE;
+using Unity.Mathematics;
+using Unity.Serialization.Json;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace FrozenAPE
+{
+    public class FrozenAPE_InstaFreezePoseExportMenu : EditorWindow
+    {
+        [MenuItem("FrozenAPE/Insta Pose, Freeze and Export from JSON...")]
+        [MenuItem("GameObject/FrozenAPE/Insta Pose, Freeze and Export from JSON...")]
+        private static void InstaPoseFreezeExportFromJson(MenuCommand menuCommand)
+        {
+            FrozenAPE_PoseMenu.PoseFromJson(menuCommand);
+            FrozenAPE_FreezePoseMenu.FreezeCurrentPose(menuCommand);
+            FrozenAPE_Menu.ExportOBJ(menuCommand);
+        }
+    }
+}
