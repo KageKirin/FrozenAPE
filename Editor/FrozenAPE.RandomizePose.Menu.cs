2ca52434cb9883e56479c44c28cf2db08c67b064
Christian Helmich <kagekirin@gmail.com>
2024-06-19 04:21:26 +0200

    feature (Editor): implement 'FrozenAPE/Randomize Pose' menu functionality (feature/editor-randomize-pose-menu) [#33]
    - **feature (Runtime): extend IRigPuppeteer interface with RandomizePose()**
    - **feature (Runtime): implement IRigPuppeteer.RandomizePose() in RigPuppeteer**
    - **feature (Editor): implement 'FrozenAPE/Randomize Pose' menu functionality**

---
+++ Editor/FrozenAPE.RandomizePose.Menu.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using FrozenAPE;
+using Unity.Mathematics;
+using Unity.Serialization.Json;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace FrozenAPE
+{
+    public class FrozenAPE_RandomizePoseMenu : EditorWindow
+    {
+        [MenuItem("FrozenAPE/Randomize Pose")]
+        [MenuItem("GameObject/FrozenAPE/Randomize Pose")]
+        private static void RandomizePose(MenuCommand menuCommand)
+        {
+            if (Selection.activeObject == null)
+            {
+                Debug.LogError("Nothing selected! Please select a GameObject.");
+                return;
+            }
+
+            GameObject go = (GameObject)Selection.activeObject;
+            if (go == null)
+            {
+                Debug.LogError("Only GameObjects can be posed. Please select a GameObject.");
+                return;
+            }
+
+            IRigPuppeteer rigPuppeteer = new RigPuppeteer();
+            rigPuppeteer.RandomizePose(go.GetComponentsInChildren<Transform>(true));
+        }
+    }
+}
