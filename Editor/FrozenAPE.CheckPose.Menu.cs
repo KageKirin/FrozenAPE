02a41b20f6e85d14f7903c60fb604a04cafdd123
Christian Helmich <kagekirin@gmail.com>
2024-07-23 04:53:03 +0200

    feature (Editor): implement 'FrozenAPE/Check Pose against JSON...' menu functionality (feature/check-pose) [#36]
    - **feature (Runtime): define interface IRigVerifier**
    - **feature (Runtime): implement interface IRigVerifier as RigVerifier**
    - **feature (Editor): implement 'FrozenAPE/Check Pose against JSON...' menu functionality**

---
+++ Editor/FrozenAPE.CheckPose.Menu.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using FrozenAPE;
+using Unity.Mathematics;
+using Unity.Serialization.Json;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace FrozenAPE
+{
+    public class FrozenAPE_CheckPoseMenu : EditorWindow
+    {
+        [MenuItem("FrozenAPE/Check Pose against JSON...")]
+        [MenuItem("GameObject/FrozenAPE/Check Pose against JSON...")]
+        private static void CheckPoseJson(MenuCommand menuCommand)
+        {
+            if (Selection.activeObject == null)
+            {
+                Debug.LogError("Nothing selected! Please select a GameObject.");
+                return;
+            }
+
+            GameObject go = (GameObject)Selection.activeObject;
+            if (go == null)
+            {
+                Debug.LogError("Only GameObjects can be posed. Please select a GameObject.");
+                return;
+            }
+
+            var path = EditorUtility.OpenFilePanel("Select pose JSON", string.Empty, "json");
+            if (string.IsNullOrEmpty(path))
+            {
+                Debug.LogError("No file selected.");
+                return;
+            }
+
+            var json = File.ReadAllText(path);
+            if (string.IsNullOrEmpty(json))
+            {
+                Debug.LogError($"Failed to read {path}.");
+                return;
+            }
+
+            var posedBones = JsonSerialization.FromJson<PosedBoneContainer>(json);
+            if (posedBones.bones.Count == 0)
+            {
+                Debug.LogError($"Could not read posed bones in {path}.");
+                return;
+            }
+
+            IRigVerifier rigVerifier = new RigVerifier();
+            bool checkOk = rigVerifier.CheckPose(go.GetComponentsInChildren<Transform>(true), posedBones.bones);
+            if (checkOk)
+            {
+                Debug.Log("current pose and JSON data match");
+            }
+            else
+            {
+                Debug.LogError("current pose and JSON data DO NOT match");
+            }
+        }
+    }
+}
