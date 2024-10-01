7181be0c6ab709406834f0e5b3b09a5ab0b09b90
Christian Helmich <kagekirin@gmail.com>
2024-10-01 14:44:06 +0900

    refactor (RigVerifier): remove superfluous method implementations (refactor/remove-copy-pasta-residual-in-rig-verifier-implementation) [#52]
    - **refactor (RigVerifier): remove implementation for superfluous method .RandomizePose()**
    - **refactor (RigVerifier): remove implementation for superfluous method .SavePose()**

--- Runtime/RigVerifier.cs
+++ Runtime/RigVerifier.cs
@@ -93,40 +93,5 @@ namespace FrozenAPE
 
             return matches.All(b => b);
         }
-
-        /// <summary>
-        /// saves the provided transforms into posed bones
-        /// </summary>
-        /// <param name="transforms">transforms as returned by `<![CDATA[GameObject.GetComponentsInChildren<Transform>(true)]]>`</param>
-        /// <param name="posedBones">the transforms as posed bones</param>
-        public virtual void SavePose(Transform[] transforms, out IEnumerable<PosedBone> posedBones)
-        {
-            List<PosedBone> posedBoneList = new();
-
-            foreach (var transform in transforms)
-            {
-                PosedBone posedBone = new();
-                posedBone.targetBone = transform.name;
-                posedBone.rotation = (float3)transform.eulerAngles;
-                posedBone.position = (float3)transform.position;
-                posedBone.scaling = (float3)transform.localScale;
-
-                posedBoneList.Add(posedBone);
-            }
-
-            posedBones = posedBoneList;
-        }
-
-        public virtual void RandomizePose(Transform[] transforms)
-        {
-            Unity.Mathematics.Random random = new((uint)DateTimeOffset.UtcNow.ToUnixTimeSeconds());
-
-            foreach (var transform in transforms)
-            {
-                transform.eulerAngles = (float3)transform.eulerAngles + random.NextFloat3(-30, 30);
-                transform.position = (float3)transform.position + random.NextFloat3(-0.1f, 0.1f);
-                transform.localScale = (float3)transform.localScale * random.NextFloat3(0.9f, 1.1f);
-            }
-        }
     }
 }
