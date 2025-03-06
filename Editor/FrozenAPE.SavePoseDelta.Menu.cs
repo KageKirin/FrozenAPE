2e52199b0925973664ffee99ae6a31940bca7b04
Christian Helmich <kagekirin@gmail.com>
2025-03-07 02:52:25 +0900

    fix (Editor): set correct conditions for writing transform parts to delta in 'Save Pose Δ to JSON' menu (fix/delta-pose-logic) [#78]
    - **fix (Editor): set correct condition for writing position to delta in 'Save Pose Δ to JSON' menu**
    - **fix (Editor): set correct condition for writing rotation to delta in 'Save Pose Δ to JSON' menu**
    - **fix (Editor): set correct condition for writing scaling to delta in 'Save Pose Δ to JSON' menu**

--- Editor/FrozenAPE.SavePoseDelta.Menu.cs
+++ Editor/FrozenAPE.SavePoseDelta.Menu.cs
@@ -90,7 +90,7 @@ namespace FrozenAPE
                     double3 delta = (double3)(dictPosedBones[key].position) - (double3)(refDictPosedBones[key].position);
                     delta = math.double3(Math.Round(delta.x, 7), Math.Round(delta.y, 7), Math.Round(delta.z, 7));
 
-                    if (math.all(delta != double3.zero))
+                    if (math.any(delta != double3.zero))
                         deltaBone.position = delta;
                 }
 
@@ -99,7 +99,7 @@ namespace FrozenAPE
                     double3 delta = (double3)(dictPosedBones[key].rotation) - (double3)(refDictPosedBones[key].rotation);
                     delta = math.double3(Math.Round(delta.x, 7), Math.Round(delta.y, 7), Math.Round(delta.z, 7));
 
-                    if (math.all(delta != double3.zero))
+                    if (math.any(delta != double3.zero))
                         deltaBone.rotation = delta;
                 }
 
@@ -108,7 +108,7 @@ namespace FrozenAPE
                     double3 delta = (double3)(dictPosedBones[key].scaling) - (double3)(refDictPosedBones[key].scaling);
                     delta = math.double3(Math.Round(delta.x, 7), Math.Round(delta.y, 7), Math.Round(delta.z, 7));
 
-                    if (math.all(delta != double3.zero))
+                    if (math.any(delta != double3.zero))
                         deltaBone.scaling = delta;
                 }
 
