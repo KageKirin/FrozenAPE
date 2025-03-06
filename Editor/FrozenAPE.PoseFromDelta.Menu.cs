8127a6f58ba51f677de5f23cb496a95df299888b
Chris 'KageKirin' Helmich <kagekirin@gmail.com>
2025-03-06 14:45:52 +0900

    feature (Editor): implement 'FrozenAPE/Pose in WORLD SPACE from ΔJSON' menu functionality

--- Editor/FrozenAPE.PoseFromDelta.Menu.cs
+++ Editor/FrozenAPE.PoseFromDelta.Menu.cs
@@ -55,4 +55,48 @@ namespace FrozenAPE
             IRigPuppeteer rigPuppeteer = new RigPuppeteer();
             rigPuppeteer.PoseFromDelta(go.GetComponentsInChildren<Transform>(true), posedBones.bones);
         }
+
+        [MenuItem("FrozenAPE/Pose in WORLD SPACE from ΔJSON...")]
+        [MenuItem("GameObject/FrozenAPE/Pose in WORLD SPACE from ΔJSON...")]
+        // [MenuItem("Assets/FrozenAPE/Pose in WORLD SPACE from ΔJSON...")] //< assets are readonly
+        public static void PoseInWorldSpaceFromDeltaJson(MenuCommand menuCommand)
+        {
+            if (Selection.activeObject == null)
+            {
+                Debug.LogError("Nothing selected! Please select a GameObject.");
+                return;
+            }
+
+            GameObject go = (GameObject)Selection.activeObject;
+            if (go == null)
+            {
+                Debug.LogError("Only GameObjects can be posed. Please select a GameObject.");
+                return;
+            }
+
+            var path = EditorUtility.OpenFilePanel("Load Δpose from JSON", string.Empty, "json");
+            if (string.IsNullOrEmpty(path))
+            {
+                Debug.LogError("No file selected.");
+                return;
+            }
+
+            var json = File.ReadAllText(path);
+            if (string.IsNullOrEmpty(json))
+            {
+                Debug.LogError($"Failed to read {path}.");
+                return;
+            }
+
+            var posedBones = JsonSerialization.FromJson<PosedBoneContainer>(json);
+            if (posedBones.bones.Count == 0)
+            {
+                Debug.LogError($"Could not read posed bones in {path}.");
+                return;
+            }
+
+            IRigPuppeteer rigPuppeteer = new RigPuppeteer();
+            rigPuppeteer.PoseFromDeltaInWorldSpace(go.GetComponentsInChildren<Transform>(true), posedBones.bones);
+        }
+    }
 }
