d80fe3ce9b5ce197f4dea644ad323bb4379a62b5
Chris 'KageKirin' Helmich <kagekirin@gmail.com>
2025-03-07 02:47:23 +0900

    fix (Editor): set correct condition for writing rotation to delta in 'Save Pose Δ to JSON' menu

--- Editor/FrozenAPE.SavePoseDelta.Menu.cs
+++ Editor/FrozenAPE.SavePoseDelta.Menu.cs
@@ -99,7 +99,7 @@ namespace FrozenAPE
                     double3 delta = (double3)(dictPosedBones[key].rotation) - (double3)(refDictPosedBones[key].rotation);
                     delta = math.double3(Math.Round(delta.x, 7), Math.Round(delta.y, 7), Math.Round(delta.z, 7));
 
-                    if (math.all(delta != double3.zero))
+                    if (math.any(delta != double3.zero))
                         deltaBone.rotation = delta;
                 }
 
