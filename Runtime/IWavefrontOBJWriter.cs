3aa95e8f14ecb69b84889580d7800d17aa3e3d7f
Chris 'KageKirin' Helmich <kagekirin@gmail.com>
2025-03-09 17:38:29 +0900

    feature (Runtime): define IWavefrontMTLWriter.WriteOBJ() interface for multiple meshes

--- Runtime/IWavefrontOBJWriter.cs
+++ Runtime/IWavefrontOBJWriter.cs
@@ -13,5 +13,15 @@ namespace FrozenAPE
         /// <param name="materials">array of Unity Material data, usually obtained from `MeshRenderer.sharedMaterials` or `SkinnedMeshRenderer.sharedMaterials`</param>
         /// <returns>string containing the OBJ (ascii) representation</returns>
         string WriteOBJ(string name, Mesh mesh, Material[] materials);
+
+        /// <summary>
+        /// create the OBJ (ascii) representation from several provided meshes and and their respective materials
+        /// this allows to to merge multiple meshes into 1, provided their vertices fit.
+        /// </summary>
+        /// <param name="name">name given to this model. Note: it must match the name provided to the MTL file as well as the filename written to disc</param>
+        /// <param name="meshes">array Unity Mesh data, usually obtained from `MeshFilter.sharedMesh` or `SkinnedMeshRenderer.sharedMesh`</param>
+        /// <param name="materials">array of Unity Material data, usually obtained from `MeshRenderer.sharedMaterials` or `SkinnedMeshRenderer.sharedMaterials`</param>
+        /// <returns>string containing the OBJ (ascii) representation</returns>
+        string WriteOBJ(string name, Mesh[] meshes, Material[] materials);
     }
 }
